99e23c82a8fa684528f11004b83d8f8650363425
mpapili <michaelpapili@gmail.com>
2024-04-21 22:56:16 -0400

    some actual success

--- kubevirt-guided/start_minikube.sh
+++ kubevirt-guided/start_minikube.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
-minikube start --cni=flannel
+# start minikube setting profile to kubevirt
+minikube start -p kubevirt --cni=flannel --memory 16384 --disk-size='50000mb'
+# switch to kubevirt profile
+minikube profile kubevirt
+# enable the kubevirt addon for minikube
+minikube addons enable kubevirt
 
