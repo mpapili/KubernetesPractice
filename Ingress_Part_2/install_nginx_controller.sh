349d24f925e0316682a7b2400c9a12b5a07876ce
mpapili <michaelpapili@gmail.com>
2024-07-26 09:32:14 -0400

    ingress review

---
+++ Ingress_Part_2/install_nginx_controller.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
---
+++ Ingress_Part_3/tester.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# replace the Host with the domain that your ingress expects
+curl 10.110.115.213 -H "Host: rocky9-server-2.nip.io"
